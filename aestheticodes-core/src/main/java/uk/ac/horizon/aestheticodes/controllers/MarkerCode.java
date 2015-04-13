ffc6a3079eb733840fdc7623cb2918b59d950a1a
wwpreston <Will@Williams-MacBook-Pro.local>
2015-04-13 19:11:45 +0100

    Copyright update

--- aestheticodes-core/src/main/java/uk/ac/horizon/aestheticodes/controllers/MarkerCode.java
+++ aestheticodes-core/src/main/java/uk/ac/horizon/aestheticodes/controllers/MarkerCode.java
@@ -1,7 +1,7 @@
 /*
  * Aestheticodes recognises a different marker scheme that allows the
  * creation of aesthetically pleasing, even beautiful, codes. 
- * Copyright (C) 2015  Aestheticodes
+ * Copyright (C) 2013-2015  The University of Nottingham
  *
  *     This program is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU Affero General Public License as published
