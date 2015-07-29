52c6f594048dcb7c0bca580ea8ad3e2aa4c89040
Kevin Glover <ktg@cs.nott.ac.uk>
2015-07-29 15:57:18 +0100

    Lots more refactoring. I should commit more often.

---
+++ src/main/java/uk/ac/horizon/artcodes/activity/ExperienceHistoryActivity.java
@@ -0,0 +1,13 @@
+package uk.ac.horizon.artcodes.activity;
+
+import uk.ac.horizon.artcodes.model.Experience;
+import uk.ac.horizon.artcodes.scanner.activity.ExperienceActivityBase;
+
+public class ExperienceHistoryActivity extends ExperienceActivityBase
+{
+	@Override
+	public void onItemChanged(Experience experience)
+	{
+		super.onItemChanged(experience);
+	}
+}
