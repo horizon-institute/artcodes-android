f1b5d8a03ab6bb21a635db6ca831fbd67089fe91
Kevin Glover <ktg@cs.nott.ac.uk>
2016-06-15 12:24:01 +0100

    v3.2.3

--- artcodes-scanner/src/main/java/uk/ac/horizon/artcodes/scanner/ScannerActivity.java
+++ artcodes-scanner/src/main/java/uk/ac/horizon/artcodes/scanner/ScannerActivity.java
@@ -20,6 +20,7 @@
 package uk.ac.horizon.artcodes.scanner;
 
 import android.Manifest;
+import android.annotation.TargetApi;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.res.ColorStateList;
@@ -79,6 +80,15 @@ public class ScannerActivity extends AppCompatActivity
 		menuAnimator.hideView();
 	}
 
+	@TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+	private void hideSystemUI()
+	{
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN)
+		{
+			getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+		}
+	}
+
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
@@ -86,10 +96,7 @@ public class ScannerActivity extends AppCompatActivity
 		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 		getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT)
-		{
-			getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
-		}
+		hideSystemUI();
 
 		setContentView(R.layout.scanner);
 
